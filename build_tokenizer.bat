ab203bf801a2ec5ab2b4007fbdfeaf2d26d8adc0
rhoeberg <contact@rhoeberg.com>
2022-01-06 23:58:13 +0100

    testing fork

--- build_tokenizer.bat
+++ build_tokenizer.bat
@@ -1,6 +1,7 @@
 SET VC_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community
 IF NOT DEFINED LIB (IF EXIST "%VC_PATH%" (call "%VC_PATH%\VC\Auxiliary\Build\vcvars32.bat" %1))
 
+
 cl code\meta.cpp /Femeta.exe
 .\meta.exe
 DEL .\meta.exe
