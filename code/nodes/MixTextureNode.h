6748e545a78b168b58f426a72080684f27e28be5
rhoeberg <contact@rhoeberg.com>
2022-02-04 17:00:53 +0100

    added support for different size input textures

--- code/nodes/MixTextureNode.h
+++ code/nodes/MixTextureNode.h
@@ -1,36 +1,73 @@
 #pragma once
 
+/*
+  This node mixes two textures
+  
+  for now only does a simple stretch algorithm using truncation to handle different 
+  size textures.
+  
+  the order parameter decides which texture is used as the final output size. 
+  0 = the first texture (default)
+  not 0 = the second texture 
+  
+ */
 void MixTextureOperation(Node *self)
 {
-	// GET INPUTS
-	Texture *tex1 = GetTextures()->Get(self->inputs[0]);
-	Texture *tex2 = GetTextures()->Get(self->inputs[1]);
-	if(!tex1 || !tex2)
-		return;
+	// GET PARAMETERS
+	double mix = self->params[0].Double();
+	i32 order = self->params[1].Int();
 
-	if(tex1->width != tex2->width && 
-	   tex1->height != tex2->height) {
-		DebugLog("Mix Texture cannot use two different size textures\n");
+	// GET INPUTS
+	Texture *tex1 = NULL;
+	Texture *tex2 = NULL;
+	if(order == 0) {
+		tex1 = GetTextures()->Get(self->inputs[0]);
+		tex2 = GetTextures()->Get(self->inputs[1]);
+	}
+	else {
+		tex1 = GetTextures()->Get(self->inputs[1]);
+		tex2 = GetTextures()->Get(self->inputs[0]);
+	}
+
+	if(!tex1 || !tex2) {
+		ErrorLog("MixTextureNode: could not find inputs");
 		return;
 	}
 
-	// GET SELF
-	// TODO (rhoe) make cleaner way to get parameters
-	double mix = self->params[0].Double();
 	Texture *output = GetTextures()->Get(self->GetDataLast());
+	if(!output) {
+		ErrorLog("MixTextureNode: could not find output");
+		return;
+	}
+
 	output->Create(tex1->width, tex1->height);
 
 	// OPERATION
 	float inputMul1 = 1.0f - mix;
 	float inputMul2 = mix;
+	float stepX = (float)tex2->width / (float)tex1->width;
+	float stepY = (float)tex2->height / (float)tex1->height;
 	for(int x = 0; x < output->width; x++) {
-		for(int y = 0; y < output->height; y++) {
-			int index = GetPixelIndex(x, y, output->height);
+		for(i32 y = 0; y < output->height; y++) {
+			i32 index1 = GetPixelIndex(x, y, tex1->width);
+
+			i32 tex2X = (i32)(x * stepX);
+			i32 tex2Y = (i32)(y * stepY);
+
+			i32 index2 = GetPixelIndex(tex2X, tex2Y, tex2->width);
 			Pixel result = {};
-			result.r = ((tex1->pixels[index].r * inputMul1) + (tex2->pixels[index].r * inputMul2));
-			result.g = ((tex1->pixels[index].g * inputMul1) + (tex2->pixels[index].g * inputMul2));
-			result.b = ((tex1->pixels[index].b * inputMul1) + (tex2->pixels[index].b * inputMul2));
-			output->pixels[index] = result;
+
+			/* if(x > (tex2->width - 1) || */
+			/*    y > (tex2->height - 1)) { */
+			/* 	result = tex1->pixels[index1]; */
+			/* } */
+			/* else { */
+			result.r = ((tex1->pixels[index1].r * inputMul1) + (tex2->pixels[index2].r * inputMul2));
+			result.g = ((tex1->pixels[index1].g * inputMul1) + (tex2->pixels[index2].g * inputMul2));
+			result.b = ((tex1->pixels[index1].b * inputMul1) + (tex2->pixels[index2].b * inputMul2));
+			/* } */
+
+			output->pixels[index1] = result;
 		}
 	}
 }
@@ -39,6 +76,7 @@ ObjectHandle CreateMixTexture()
 {
 	FixedArray<NodeParameter> params = {
 		NodeParameter("mix", 0.5),
+		NodeParameter("order", 0),
 	};
 
 	FixedArray<NodeInput> inputs = {
